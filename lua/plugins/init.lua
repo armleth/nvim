2ee0b435f3838e1f0b3a43394c74746895837ec1
Armand THIBAUDON <armand.thibaudon@stockly.ai>
2024-09-19 12:05:41 +0200

    noice + session setup begin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,14 +3,36 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     'nvim-tree/nvim-web-devicons',
 
-    {
-        'stevearc/resession.nvim',
-        keys = {
-            { "<leader>ss", function() require("resession").save() end, desc = "Save Session" },
-            { "<leader>sl", function() require("resession").load() end, desc = "Load Session" },
-            { "<leader>sd", function() require("resession").delete() end, desc = "Delete Session" },
-        },
-    },
+    { 'numToStr/Comment.nvim', opts = {} },
+
+    -- {
+    --     'stevearc/resession.nvim',
+    --     keys = {
+    --         { "<leader>ss", function() require("resession").save() end, desc = "Save Session" },
+    --         { "<leader>sl", function() require("resession").load() end, desc = "Load Session" },
+    --         { "<leader>sd", function() require("resession").delete() end, desc = "Delete Session" },
+    --     },
+    -- },
+
+    -- {
+    --     "folke/persistence.nvim",
+    --     event = "BufReadPre",
+    --     opts = {},
+    --
+    --     config = function ()
+    --         -- load the session for the current directory
+    --         vim.keymap.set("n", "<leader>qs", function() require("persistence").load() end)
+    --
+    --         -- select a session to load
+    --         vim.keymap.set("n", "<leader>qS", function() require("persistence").select() end)
+    --
+    --         -- load the last session
+    --         vim.keymap.set("n", "<leader>ql", function() require("persistence").load({ last = true }) end)
+    --
+    --         -- stop Persistence => session won't be saved on exit
+    --         vim.keymap.set("n", "<leader>qd", function() require("persistence").stop() end)
+    --     end
+    -- },
 
     {
         'mrjones2014/smart-splits.nvim',
@@ -50,8 +72,38 @@ return {
         options = { try_as_border = true },
     },
 
-    { 'numToStr/Comment.nvim', opts = {} },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
 
+        config = function ()
+            require("noice").setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+            })
+        end
+    },
 
     { -- Extensible UI for Neovim notifications and LSP progress messages.
         'j-hui/fidget.nvim',
