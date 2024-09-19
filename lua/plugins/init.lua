e3a40d6623f3fe2c2d6ec447a1de907fa6563d37
Armand THIBAUDON <armand.thibaudon@stockly.ai>
2024-09-19 12:31:08 +0200

    style

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,7 +18,7 @@ return {
             { '<leader>sa', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
         },
 
-       ---enables autocomplete for opts
+        ---enables autocomplete for opts
         opts = {
             -- ⚠️ This will only work if Telescope.nvim is installed
             -- The following are already the default values, no need to provide them if these are already the settings you want.
@@ -115,170 +115,170 @@ return {
     },
 
     { -- Extensible UI for Neovim notifications and LSP progress messages.
-        'j-hui/fidget.nvim',
-        opts = {
-            notification = {
-                window = {
-                    winblend = 0,
-                },
+    'j-hui/fidget.nvim',
+    opts = {
+        notification = {
+            window = {
+                winblend = 0,
             },
-            progress = {
-                lsp = {
-                    progress_ringbuf_size = 4096,
-                },
+        },
+        progress = {
+            lsp = {
+                progress_ringbuf_size = 4096,
             },
         },
     },
+},
 
-    -- Highlight todo, notes, etc in comments
-    { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+-- Highlight todo, notes, etc in comments
+{ 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
-    { 'windwp/nvim-autopairs', event = 'InsertEnter', config = true, },
+{ 'windwp/nvim-autopairs', event = 'InsertEnter', config = true, },
 
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+{
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
 
-        config = function ()
-            require('lualine').setup {
-                options = {
-                    icons_enabled = true,
-                    theme = 'auto',
-                    -- component_separators = { left = '', right = ''},
-                    -- section_separators = { left = '', right = ''},
-                    component_separators = { left = '', right = '' },
-                    section_separators = { left = '', right = '' },
-                    disabled_filetypes = {
-                        statusline = {},
-                        winbar = {},
-                    },
-                    ignore_focus = {},
-                    always_divide_middle = true,
-                    globalstatus = false,
-                    refresh = {
-                        statusline = 1000,
-                        tabline = 1000,
-                        winbar = 1000,
-                    }
+    config = function ()
+        require('lualine').setup {
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+                -- component_separators = { left = '', right = ''},
+                -- section_separators = { left = '', right = ''},
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
                 },
-                sections = {
-                    lualine_a = {'mode'},
-                    lualine_b = {'branch', 'diff', 'diagnostics'},
-                    lualine_c = {'filename'},
-                    lualine_x = {'encoding', 'fileformat', 'filetype'},
-                    lualine_y = {'progress'},
-                    lualine_z = {'location'}
-                },
-                inactive_sections = {
-                    lualine_a = {},
-                    lualine_b = {},
-                    lualine_c = {'filename'},
-                    lualine_x = {'location'},
-                    lualine_y = {},
-                    lualine_z = {}
-                },
-                tabline = {},
-                winbar = {},
-                inactive_winbar = {},
-                extensions = {},
-            }
-        end
+                ignore_focus = {},
+                always_divide_middle = true,
+                globalstatus = false,
+                refresh = {
+                    statusline = 1000,
+                    tabline = 1000,
+                    winbar = 1000,
+                }
+            },
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_c = {'filename'},
+                lualine_x = {'encoding', 'fileformat', 'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'}
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {'filename'},
+                lualine_x = {'location'},
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {},
+        }
+    end
+},
+
+{
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        'nvim-telescope/telescope.nvim',
     },
 
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            'nvim-telescope/telescope.nvim',
-        },
+    config = function ()
+        local harpoon = require("harpoon")
 
-        config = function ()
-            local harpoon = require("harpoon")
+        harpoon:setup({})
 
-            harpoon:setup({})
+        vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end)
 
-            vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end)
+        for i = 1, 5 do
+            vim.keymap.set("n", "<leader>" .. i, function() harpoon:list():select(i) end)
+        end
 
-            for i = 1, 5 do
-                vim.keymap.set("n", "<leader>" .. i, function() harpoon:list():select(i) end)
+        -- basic telescope configuration
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
             end
 
-            -- basic telescope configuration
-            local conf = require("telescope.config").values
-            local function toggle_telescope(harpoon_files)
-                local file_paths = {}
-                for _, item in ipairs(harpoon_files.items) do
-                    table.insert(file_paths, item.value)
-                end
-
-                require("telescope.pickers").new({}, {
-                    prompt_title = "Harpoon",
-                    finder = require("telescope.finders").new_table({
-                        results = file_paths,
-                    }),
-                    previewer = conf.file_previewer({}),
-                    sorter = conf.generic_sorter({}),
-                }):find()
-            end
-
-            vim.keymap.set("n", "<leader>jk", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
-
-            -- Toggle previous & next buffers stored within Harpoon list
-            -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-            -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+            }):find()
         end
-    },
 
-    {
-        'stevearc/aerial.nvim',
-        opts = {},
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons"
-        } ,
+        vim.keymap.set("n", "<leader>jk", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
 
-        -- TODO: folding + go on it on load
-        config = function ()
-            require("aerial").setup({
-                layout = {
-                    -- These control the width of the aerial window.
-                    -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-                    -- min_width and max_width can be a list of mixed types.
-                    -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
-                    max_width = { 40, 0.2 },
-                    width = nil,
-                    min_width = 25,
+        -- Toggle previous & next buffers stored within Harpoon list
+        -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+        -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+    end
+},
 
-                    -- key-value pairs of window-local options for aerial window (e.g. winhl)
-                    win_opts = {},
+{
+    'stevearc/aerial.nvim',
+    opts = {},
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
+    } ,
 
-                    -- Determines the default direction to open the aerial window. The 'prefer'
-                    -- options will open the window in the other direction *if* there is a
-                    -- different buffer in the way of the preferred direction
-                    -- Enum: prefer_right, prefer_left, right, left, float
-                    default_direction = "prefer_left",
+    -- TODO: folding + go on it on load
+    config = function ()
+        require("aerial").setup({
+            layout = {
+                -- These control the width of the aerial window.
+                -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+                -- min_width and max_width can be a list of mixed types.
+                -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+                max_width = { 40, 0.2 },
+                width = nil,
+                min_width = 25,
 
-                    -- Determines where the aerial window will be opened
-                    --   edge   - open aerial at the far right/left of the editor
-                    --   window - open aerial to the right/left of the current window
-                    placement = "window",
+                -- key-value pairs of window-local options for aerial window (e.g. winhl)
+                win_opts = {},
 
-                    -- When the symbols change, resize the aerial window (within min/max constraints) to fit
-                    resize_to_content = true,
+                -- Determines the default direction to open the aerial window. The 'prefer'
+                -- options will open the window in the other direction *if* there is a
+                -- different buffer in the way of the preferred direction
+                -- Enum: prefer_right, prefer_left, right, left, float
+                default_direction = "prefer_left",
 
-                    -- Preserve window size equality with (:help CTRL-W_=)
-                    preserve_equality = false,
-                },
+                -- Determines where the aerial window will be opened
+                --   edge   - open aerial at the far right/left of the editor
+                --   window - open aerial to the right/left of the current window
+                placement = "window",
 
-                on_attach = function(bufnr)
-                    -- Jump forwards/backwards with '{' and '}'
-                    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-                    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-                end,
-            })
+                -- When the symbols change, resize the aerial window (within min/max constraints) to fit
+                resize_to_content = true,
 
-            vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
-        end
-    },
+                -- Preserve window size equality with (:help CTRL-W_=)
+                preserve_equality = false,
+            },
+
+            on_attach = function(bufnr)
+                -- Jump forwards/backwards with '{' and '}'
+                vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+                vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+            end,
+        })
+
+        vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+    end
+},
 }
