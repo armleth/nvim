7f3531f9ed5641d64aabf16d056db694fa3a2873
Armand THIBAUDON <armand.thibaudon@stockly.ai>
2024-09-19 14:51:34 +0200

    can now delete a harpoon mark from telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -212,13 +212,39 @@ return {
                 table.insert(file_paths, item.value)
             end
 
+            local make_finder = function()
+                local paths = {}
+
+                for _, item in ipairs(harpoon_files.items) do
+                    table.insert(paths, item.value)
+                end
+
+                return require("telescope.finders").new_table({
+                    results = paths,
+                })
+            end
+
             require("telescope.pickers").new({}, {
                 prompt_title = "Harpoon",
-                finder = require("telescope.finders").new_table({
-                    results = file_paths,
-                }),
+                finder = make_finder(),
                 previewer = conf.file_previewer({}),
                 sorter = conf.generic_sorter({}),
+
+                attach_mappings = function(prompt_buffer_number, map)
+                    -- The keymap you need
+                    map("i", "<c-d>", function()
+                        local state = require("telescope.actions.state")
+                        local selected_entry = state.get_selected_entry()
+                        local current_picker = state.get_current_picker(prompt_buffer_number)
+
+                        -- This is the line you need to remove the entry
+                        harpoon:list():remove(selected_entry)
+                        current_picker:refresh(make_finder())
+                    end)
+
+                    return true
+                end,
+
             }):find()
         end
 
