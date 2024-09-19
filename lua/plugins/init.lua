b628442ba58fb631c455da2be515cfad5e8c4a5d
Armand THIBAUDON <armand.thibaudon@stockly.ai>
2024-09-19 12:13:32 +0200

    sessions workings

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,34 +5,43 @@ return {
 
     { 'numToStr/Comment.nvim', opts = {} },
 
-    -- {
-    --     'stevearc/resession.nvim',
-    --     keys = {
-    --         { "<leader>ss", function() require("resession").save() end, desc = "Save Session" },
-    --         { "<leader>sl", function() require("resession").load() end, desc = "Load Session" },
-    --         { "<leader>sd", function() require("resession").delete() end, desc = "Delete Session" },
-    --     },
-    -- },
+    {
+        'rmagatti/auto-session',
+        lazy = false,
+        dependencies = {
+            'nvim-telescope/telescope.nvim',
+        },
+        keys = {
+            -- Will use Telescope if installed or a vim.ui.select picker otherwise
+            { '<leader>sl', '<cmd>SessionSearch<CR>', desc = 'Session search' },
+            { '<leader>ss', '<cmd>SessionSave<CR>', desc = 'Save session' },
+            { '<leader>sa', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
+        },
 
-    -- {
-    --     "folke/persistence.nvim",
-    --     event = "BufReadPre",
-    --     opts = {},
-    --
-    --     config = function ()
-    --         -- load the session for the current directory
-    --         vim.keymap.set("n", "<leader>qs", function() require("persistence").load() end)
-    --
-    --         -- select a session to load
-    --         vim.keymap.set("n", "<leader>qS", function() require("persistence").select() end)
-    --
-    --         -- load the last session
-    --         vim.keymap.set("n", "<leader>ql", function() require("persistence").load({ last = true }) end)
-    --
-    --         -- stop Persistence => session won't be saved on exit
-    --         vim.keymap.set("n", "<leader>qd", function() require("persistence").stop() end)
-    --     end
-    -- },
+       ---enables autocomplete for opts
+        opts = {
+            -- ⚠️ This will only work if Telescope.nvim is installed
+            -- The following are already the default values, no need to provide them if these are already the settings you want.
+            session_lens = {
+                -- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
+                load_on_setup = true,
+                previewer = false,
+                mappings = {
+                    -- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
+                    delete_session = { "i", "<C-D>" },
+                    alternate_session = { "i", "<C-S>" },
+                },
+                -- Can also set some Telescope picker options
+                theme_conf = {
+                    border = true,
+                    -- layout_config = {
+                    --   width = 0.8, -- Can set width and height as percent of window
+                    --   height = 0.5,
+                    -- },
+                },
+            },
+        }
+    },
 
     {
         'mrjones2014/smart-splits.nvim',
