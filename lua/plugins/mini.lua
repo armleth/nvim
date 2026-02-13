597b4e680fc58b445e9473aaa810fc1bf0fcec53
Armand Thibaudon <armand.thibaudon@datadoghq.com>
2026-02-13 17:17:01 +0100

    feat(mini): now show git diff on left col

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -30,6 +30,13 @@ return {
             },
         })
 
+        require('mini.diff').setup({
+            view = {
+                style = 'sign',
+                signs = { add = '│', change = '│', delete = '_' },
+            },
+        })
+
         require('mini.sessions').setup({
             autoread = true,
             autowrite = true,
